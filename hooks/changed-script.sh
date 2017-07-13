0a55535011593f751d9df93582613c74ba3d4309
Adham Helal <ahelal@users.noreply.github.com>
2017-07-13 16:24:08 +0200

    Make consul-template run idempotent

--- hooks/changed-script.sh
+++ hooks/changed-script.sh
@@ -12,21 +12,24 @@ else
     CONSUL_ADDR=localhost
 fi
 
+
 CUSTOM_SCRIPT="/hooks/preChange"
 if [ -x "${CUSTOM_SCRIPT}" ]; then
     echo "* Custom preChange script"
     ${CUSTOM_SCRIPT}
 fi
 
-echo "Run Conusl-template to generate template"
+
+# TODO: write a script that loops and checks backend that needs to be drained, disabled or goto maintaince mode
+
+
+echo "Run Conusl-template to generate template and reload if needed"
 /usr/local/bin/consul-template \
     -consul-addr ${CONSUL_ADDR}:8500  \
     -once \
     -dedup \
-    -template "/etc/haproxy.ctmpl:/etc/haproxy/haproxy.cfg"
+    -template "/etc/haproxy.ctmpl:/etc/haproxy/haproxy.cfg:/usr/local/bin/haproxy-manage.sh reload"
 
-echo "Reloading Haproxy"
-/usr/local/bin/haproxy-manage.sh reload
 
 CUSTOM_SCRIPT="/hooks/postChange"
 if [ -x "${CUSTOM_SCRIPT}" ]; then
