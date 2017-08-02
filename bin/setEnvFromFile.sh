368c14e261f74445608abf47caaa3ded378144cc
Adham <ahelal@users.noreply.github.com>
2017-08-02 11:55:08 +0200

    rewrite filter to ignore newlines fixes issue #14 (#20)

--- bin/setEnvFromFile.sh
+++ bin/setEnvFromFile.sh
@@ -1,22 +1,29 @@
 #!/bin/sh
 set -e
 
+# Set environmantal variable using container pilot
+
+
 envFile="${1}"
+tmpDir="$(mktemp -d 2> /dev/null || mktemp -d -t 'mytmpdir')"
+tmpFile="${tmpDir}/app_env.sh"
 
 if ! [ -f "${envFile}" ]; then
     echo "Not valid file path. ${envFile}"
     exit 1
 fi
 
+# Remove # and empty lines from envFile
+grep  -v '^\s*#' "${envFile}" | grep -v '^\s*$' > "${tmpFile}"
+
 #https://stackoverflow.com/a/12916758/3167035
 while read -r line || [ -n "$line" ]; do
-  # ignore comments
-  case "${line}" in
-      \#*)
-        echo " * Setting up env (ignoring comment)" ;;
-      *)
-        echo " * Setting up env ${line}"
-        containerpilot -config /etc/containerpilot.json5 -putenv "${line}"
-      ;;
-  esac
-done < "${envFile}"
+  # trim whitespace
+  line=$(echo "${line}" | xargs)
+  key=$(echo "${line}" | cut -f 1 -d "=")
+  # Might need to remove print line and print only the key
+  echo " * Setting up env ${key}"
+  containerpilot -config /etc/containerpilot.json5 -putenv "${line}"
+done < "${tmpFile}"
+
+rm -rf "${tmpDir}"
