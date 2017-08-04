51293f5af27f85d555fb7eed52517b8e819ed9a0
Adham Helal <ahelal@users.noreply.github.com>
2017-08-04 12:36:27 +0200

    remove stop script

--- hooks/stop-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-## This script is called to stop the app
-set -e
-
-# shellcheck disable=SC1091
-[ -z "${HELPER_IMPORTED+x}" ] && . /usr/local/bin/helper.sh
-
-# Execute Hook stop if exists
-execIfExists "/hooks/stop" "* Custom stop script"
