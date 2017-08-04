4cbee2e0d527e451da79a5f410c64a7a9f8857cc
Adham Helal <ahelal@users.noreply.github.com>
2017-08-04 14:48:08 +0200

    timeout fix

--- bin/consul-leave.sh
+++ bin/consul-leave.sh
@@ -4,5 +4,5 @@ if pgrep cosnul > /dev/null; then
     echo "* Issuing Consul leave command"
     consul leave
 else
-       echo "* [warning] Consul is not running can't issue a leave command"
+    echo "* [warning] Consul is not running can't issue a leave command"
 fi
